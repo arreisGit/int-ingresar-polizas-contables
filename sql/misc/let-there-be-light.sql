06e499499bfbdfcf81c67d0863d8cd957340ab57
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-03-16 14:39:35 -0600

    Actualiza el script the liberacion

--- sql/misc/let-there-be-light.sql
+++ sql/misc/let-there-be-light.sql
@@ -1,2 +1,10 @@
 IF OBJECT_ID('dbo.CUP_SistemasCuprum', 'U') IS NOT NULL 
   DROP TABLE dbo.CUP_SistemasCuprum;
+
+IF EXISTS (SELECT name FROM sysobjects WHERE name = 'CUP_SPI_PolizasContables') 
+  DROP PROCEDURE  CUP_SPI_PolizasContables
+
+IF EXISTS (SELECT name 
+           FROM sys.xml_schema_collections
+           WHERE name='JournalEntrySchema')
+  DROP XML SCHEMA COLLECTION JournalEntrySchema
