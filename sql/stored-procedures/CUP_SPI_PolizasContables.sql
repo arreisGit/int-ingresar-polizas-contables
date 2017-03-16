d7b0d33d169046ab32687ef64cd8ed8d8d766b13
Enrique Sierra Gutierrez <enrique.sierra.0000@gmail.com>
2017-03-16 13:13:55 -0600

    Añade estructura básica para el control de los mensajes del procediiento que inserta y valida las polizas de Intelisis

--- sql/stored-procedures/CUP_SPI_PolizasContables.sql
+++ sql/stored-procedures/CUP_SPI_PolizasContables.sql
@@ -21,9 +21,35 @@ GO
 
 CREATE PROCEDURE [dbo].CUP_SPI_PolizasContables
 (
- @Poliza XML   
+ @Poliza XML(JournalEntrySchema)
 )                
 AS BEGIN   
+  DECLARE @Messages TABLE
+  (
+    Num INT NOT NULL,
+    [Description] VARCHAR(255) NOT NULL,
+    ID INT NULL,
+    Mov VARCHAR(20) NULL,
+    MovID VARCHAR(20) NULL
+  )
 
-  SELECT 'Oh yeah!'
+  INSERT INTO @Messages 
+  (
+    Num,
+  [Description]
+  )
+  VALUES
+  ( 
+    0,
+    'Oh Yeah'
+  )
+
+  SELECT
+    Num
+    ,[Description] = ISNULL([Description],'')
+    ,ID = ISNULL(CAST(ID AS VARCHAR(30)),'')
+    ,Mov = ISNULL(Mov,'')
+    ,MovID = ISNULL(MovID,'')
+  FROM
+    @Messages
 END
